972540867fcd6c9f3dbbe0bf5b9aa7dad503a8a8
Lukman <lukmaneka216@email.com>
2025-04-22 13:04:13 +0700

    feat: update structure project

---
+++ pages/register/index.tsx
@@ -0,0 +1,18 @@
+export default function Register() {
+  return (
+    <div>
+      <h1>Register</h1>
+      <form>
+        <label>
+          Username:
+          <input type="text" name="username" />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
