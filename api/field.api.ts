6a4656d3dba12d29531495ce59071845ffa4fcaa
Lukman289 <lukmaneka216@gmail.com>
2025-05-05 17:37:27 +0700

    fix: error 'branches is not a function'
    add fetching data to fix error message ' branches is not a function'

--- api/field.api.ts
+++ api/field.api.ts
@@ -1,9 +1,32 @@
 import AxiosInstance from "../lib/axiosInstance";
 
+const fields = [
+  {
+    id: 1,
+    branchId: "1",
+    typeId: "1",
+    name: "Futsal Field 1",
+    priceDay: 200000,
+    priceNight: 250000,
+    status: "available",
+    imageUrl: "https://via.placeholder.com/150",
+  },
+  {
+    id: 2,
+    branchId: "2",
+    typeId: "2",
+    name: "Futsal Field 2",
+    priceDay: 200000,
+    priceNight: 250000,
+    status: "available",
+    imageUrl: "https://via.placeholder.com/150",
+  },
+];
+
 export const getFields = async () => {
   try {
     const response = await AxiosInstance.get("/fields");
-    return response.data;
+    return (response.data == null) ? fields : response.data;
   } catch (error) {
     console.error("Error fetching fields data:", error);
     throw error;
